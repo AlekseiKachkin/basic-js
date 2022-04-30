e161cab26ad4afd8c43dfa602df4b8925031c802
AlekseiKachkin <lex6164665@yandex.ru>
2022-05-01 02:44:05 +0300

    feat: add solutin for sum-digit, dreamteam, what season, carbon dating

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let countCat = 0;  
+  matrix.forEach((item) => {
+    for( let i = 0; i < item.length; i++) {
+      if (item[i] === '^^') {
+        countCat++;
+      }
+    }
+  })
+  return countCat;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function getSeason(date) {
+  console.log(date);
+  let result = true;
+  if (date === false || date === undefined || date === null || date === '') {
+    return 'Unable to determine the time of year!';
+  }  
+
+  try {
+    let month = date.getTime();
+
+  } catch {
+    throw new Error("Invalid date!");
+  }
+
+  
+            
+      let month = date.getMonth();  
+      switch (month) {
+        case 11:
+        case 0:
+        case 1:
+          result = 'winter';
+          break;
+        case 2:
+        case 3:
+        case 4:
+          result = 'spring';
+          break;      
+        case 5:
+        case 6:
+        case 7:
+          result = 'summer';
+          break;
+        case 8:
+        case 9:
+        case 10:
+          result = 'autumn';
+          break;
+        
+        }      
+     return result;
+   
+}  
+const fakeDate = {
+  toString() {
+      return Date.prototype.toString.call(new Date());
+  },
+  [Symbol.toStringTag]: 'Date'
+};
+
+Object.setPrototypeOf(fakeDate, Object.getPrototypeOf(new Date()));
+let we = new Date(1456, 0, 2, 1, 50, 9, 238)
+console.log('fakeDate');
+console.log(fakeDate instanceof  Date);
+console.log( toString.call(we));
+
+
 
 module.exports = {
   getSeason
