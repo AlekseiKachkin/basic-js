ef89e4fd0bcbfde067a646b959fa77c451285f7c
AlekseiKachkin <lex6164665@yandex.ru>
2022-05-02 01:47:31 +0300

    feat: add solution for hanoi-tower and transform-arrayg

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,27 +5,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  chainLength: 0,
+  chain: '',  
+  chainPoints: {
+
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  getLength ()  {
+    return this.chainLength;  
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink (value) {    
+      if (this.chainLength > 0) {
+        this.chain = `${this.chain}~~( ${value} )`;
+        this.chainLength++;
+      } else {
+        this.chain = `( ${value} )`;
+        this.chainLength++;
+      }
+      return this;
   },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink (position) {
+    let start = 0,
+        startPosition,
+        end = 0,
+        endPosition;
+    for(let i = 0; i < this.chain.length; ++i) {
+      if (this.chain[i] === '(') {
+        start++;
+        startPosition = i;
+      }
+      if (this.chain[i] === ')') {
+        end++;
+        endPosition = i;
+      }
+      if (end === position){
+        break;
+      }
+    }
+    let chainStart = this.chain.slice(0, startPosition);
+    let chainEnd = this.chain.slice(endPosition+1);
+    this.chain = `${chainStart}${chainEnd}`; 
+    this.chainLength--;
+    return this;   
   },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  reverseChain () {
+    this.chain.split('').reverse().join('');
+    return this
+  },
+  
+  finishChain () {
+    return this.chain;
   }
 };
+
+
+console.log(chainMaker);
 
 module.exports = {
   chainMaker
